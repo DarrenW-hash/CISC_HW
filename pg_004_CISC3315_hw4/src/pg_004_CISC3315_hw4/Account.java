3380e73b4902f365450b7bdbfa5ff3b6439330f9
DarrenW-hash <darren1111weng@gmail.com>
2025-10-02 13:24:12 -0400

    2nd

--- pg_004_CISC3315_hw4/src/pg_004_CISC3315_hw4/Account.java
+++ pg_004_CISC3315_hw4/src/pg_004_CISC3315_hw4/Account.java
@@ -1,6 +1,7 @@
 package pg_004_CISC3315_hw4;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class Account {
 
@@ -8,10 +9,70 @@ public class Account {
 	private int AccountNumber;
 	private String accountType;
 	private boolean accountStatus;
-	private ArrayList<TransactionReceipt> transactionRecipts= new ArrayList<TransactionReceipt>();
+	private double balance;
+	private ArrayList<TransactionReceipt> transactionReceipts= new ArrayList<TransactionReceipt>();
 	private Check check;
+	private Calendar date;
 	
-	public Account(Depositor _depositor, int _AccountNumber, String _accountType, boolean _accountStatus, ArrayList transactionRecipt) {
+	public Account(Depositor _depositor, int _AccountNumber, Double _Balance, String _accountType) {
+		depositor = _depositor;
+		AccountNumber = _AccountNumber;
+		balance = _Balance;
+		accountType = _accountType;
+		//transactionReceipts = _transactionRecipt;
+	}
+	
+	//contrustor including checks
+	public Account(Depositor _depositor, int _AccountNumber, Double _Balance, String _accountType, Calendar _date) {
+		depositor = _depositor;
+		AccountNumber = _AccountNumber;
+		balance = _Balance;
+		accountType = _accountType;
+		//transactionReceipts = _transactionRecipt;
+		date = _date;
 		
+	}
+	
+	//getters
+	public Depositor getdepositor() {
+		return depositor;
+	}
+	public int getAccountNumber() {
+		return AccountNumber;
+	}
+	public String getaccountType() {
+		return accountType;
+	}
+	public boolean getAccountStatus() {
+		return accountStatus;
+	}
+	public ArrayList gettransactionRecipts() {
+		return transactionReceipts;
+	}
+	public Check getcheck() {
+		return check;
+	}
+	public double getbalance() {
+		return balance;
+	}
+	public Calendar getdate() {
+		return date;
+	}
+	
+	//setters 
+	private void setAccountStatus(boolean _accountStatus) {
+		accountStatus = _accountStatus;
+	}
+	private void setbalance(double _Balance) {
+		balance = _Balance;
+	}
+	private void addtransactionRecipt( TransactionReceipt receipt) {
+		 transactionReceipts.add(receipt);
+	}
+	private void setcheck(Check _check) {
+		check = _check;	
+	}
+	private void setCalendar(Calendar _date) {
+		date = _date;
 	}
 }
--- pg_004_CISC3315_hw4/src/pg_004_CISC3315_hw4/Bank.java
+++ pg_004_CISC3315_hw4/src/pg_004_CISC3315_hw4/Bank.java
@@ -1,9 +1,86 @@
 package pg_004_CISC3315_hw4;
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import java.util.Calendar;
+
 
 public class Bank {
 	
-	public static void main(String args []) {
+	
+	public static void main(String args []) throws IOException{
+		ArrayList<Account> bankaccounts = new ArrayList<Account>();
+		
+		ReadAccount(bankaccounts);
+		
+		int currenttotal = bankaccounts.size();
+		
+		for (int i = 0; i < currenttotal; i++) {
+		    System.out.println(bankaccounts.get(i).getdepositor().getNames().getFirstName() + " " 
+		        + bankaccounts.get(i).getdepositor().getNames().getLastName() + " "
+		        + bankaccounts.get(i).getdepositor().getSSnumber() + " "
+		        + bankaccounts.get(i).getAccountNumber() + " "
+		        + bankaccounts.get(i).getaccountType() + " "
+		        + bankaccounts.get(i).getbalance() + " "
+		        + bankaccounts.get(i).getdate());
+		 
+//		    if(bankaccounts.get(i).getaccountType() == "CD") {
+//		    	System.out.println(bankaccounts.get(i).getdepositor().getNames().getFirstName() + " " 
+//				        + bankaccounts.get(i).getdepositor().getNames().getLastName() + " "
+//				        + bankaccounts.get(i).getdepositor().getSSnumber() + " "
+//				        + bankaccounts.get(i).getAccountNumber() + " "
+//				        + bankaccounts.get(i).getaccountType() + " "
+//				        + bankaccounts.get(i).getbalance() + " "
+//		    			+ bankaccounts.get(i).getdate());
+//		    }
+		}
+		
+		
+	}
+	
+	public static void ReadAccount(ArrayList bankaccounts) throws IOException{
+		
+		
+		File bankaccountfile = new File("/Users/darrenweng/git/CISC_HW/pg_004_CISC3315_hw4/src/BankAccounts.txt");
+		Scanner fileReader = new Scanner(bankaccountfile);
+		
+		while (fileReader.hasNext()){
+			String [] line = fileReader.nextLine().split(" ");
+			//first Name
+			String token0 = line[0];
+			//last Name
+			String token1= line[1];
+			//SSnumber
+			String token2 = line[2];
+			//account Number 
+			int token3 = Integer.parseInt(line[3]);
+			//account Type
+			String token4 = line[4];
+			//double balance
+			double token5 = Double.parseDouble(line[5]);
+			
+			if(token4 == "CD") {
+				String [] Date = line[6].split("/");
+				int month = Integer.parseInt(Date[0]);
+				int day = Integer.parseInt(Date[1]);
+				int year = Integer.parseInt(Date[2]);
+				
+				Calendar maturityDate = Calendar.getInstance();
+				maturityDate.set(year, month, day);
+				
+				Name name = new Name(token0,token1);
+				Depositor depositor = new Depositor(name,token2);
+				Account account = new Account(depositor, token3,token5,token4,maturityDate);
+				bankaccounts.add(account);
+				
+	
+			}
+			Name name = new Name(token0,token1);
+			Depositor depositor = new Depositor(name,token2);
+			Account account = new Account(depositor, token3,token5,token4);
+			bankaccounts.add(account);
+		}
 		
 	}
 }
